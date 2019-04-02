afc6792d8bc05a8fae3db4efe160f1f7792778f9
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-02 13:51:30 +0700

    show data for home page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import Layout from '../components/Layout'
 import axios from 'axios'
+import Link from 'next/link'
 
 const HomePage = (props) => {
-    console.log(props.blogsData)
+    const renderBlogs = (blogs = {}) => {
+        return (
+            blogs.map(
+                blog => {
+                    return (
+                        <div key={blog.id} className="col-4">
+                            <Link href={'/blog/detail?id='+ blog.id}>
+                                <a><h4 className="mt-3">{blog.title}</h4></a>
+                            </Link>
+                            <p>{blog.body}</p>
+                            <hr/>
+                        </div>
+                    )
+                }
+            )
+        )
+    }
     return (
         <Layout title="หน้าแรก - basic Next.js">
             <div className="row">
                 <div className="col-12">
                     <h1>หน้าแรก</h1>
                 </div>
+            </div>
+            <div className="row">
+                {renderBlogs(props.blogsData)}
             </div>
         </Layout>
     )
