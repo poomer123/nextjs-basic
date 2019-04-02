a2605d5550a777c435ab815b25f9d3538d19b0fc
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-02 11:11:02 +0700

    implement head and link stylesheet

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Head from 'next/head'
 
 const HomePage = () => {
     return (
         <>
+            <Head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="theme-color" content="#000000" />
+
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="/static/css/style.css" />
+
+                <title>Next.js Basic</title>
+            </Head>
             <Header />
             Home page
             <Footer />
