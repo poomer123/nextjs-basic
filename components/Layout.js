48dfd361edc4f2c9dd47fd12c1948d5f2108dadb
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-02 12:42:21 +0700

    layout

---
+++ components/Layout.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Head from 'next/head'
+import Header from './Header'
+import Footer from './Footer'
+
+const Layout = (props) => {
+    const { children } = props
+    return (
+        <>
+            <Head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+                <meta name="theme-color" content="#000000" />
+
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" />
+                <link rel="stylesheet" href="../static/css/style.css" />
+
+                <title>Next.js Basic</title>
+            </Head>
+
+            <div className="container-fluid">
+                <Header />
+                <div className="container">
+                    {children}
+                </div>
+                <Footer />
+            </div>
+        </>
+    )
+}
+
+export default Layout
--- pages/about.js
+++ pages/about.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Layout from '../components/Layout'
 
-const HomePage = () => {
+const About = () => {
     return (
         <Layout>
             <div className="row">
                 <div className="col-12">
-                    <h1>หน้าแรก</h1>
+                    <h1>ข่าวสารต่างๆมากมาย</h1>
                 </div>
             </div>
         </Layout>
     )
 }
 
-export default HomePage
+export default About
