4985f66effea3d62955e70e5369dbac4b7cb77db
ApiratChiwakanit <apirat.c@roundtwosolutions.com>
2019-04-02 12:31:20 +0700

    fixed timer function

--- components/Header.js
+++ components/Header.js
@@ -1,32 +1,41 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import Link from 'next/link'
 
 const Header = () => {
     const [date, setDate] = useState(new Date())
 
     useEffect( () => {
-        setInterval( () => setDate( new Date() ), 1000 )
-        return () => clearInterval(timerId)
+        setTimer()
+        return () => clearInterval(setTimer())
     }, [])
 
+    function setTimer() {
+        setInterval( setDate( new Date() ), 1000 )
+    }
+
     return (
-        <div className="row mt-3">
-            <div className="col-12 col-md-4">
-                <h1 className="text-info"><img style={ {height: '50px'} } src="/static/images/logo.png" alt="Logo"/> ข่าวแจ่มแมว</h1>
+        <>
+            <div className="row mt-3">
+                <div className="col-12 col-md-4">
+                    <h1 className="text-info">
+                        <img style={ {height: '50px'} } src="/static/images/logo.png" alt="Logo"/> ข่าวแจ่มแมว
+                    </h1>
+                </div>
+                <div className="col-12 col-12 col-md-4 col-lg-7 text-right">
+                    <ul className="list-inline pt-3">
+                        <li className="list-inline-item title"><Link href="/">หน้าแรก</Link></li>
+                        <li className="list-inline-item title">|</li>
+                        <li className="list-inline-item title"><Link href="/about">เกี่ยวกับเรา</Link></li>
+                    </ul>
+                </div>
+                <div className="col-12 col-md-4 col-lg-1 text-right">
+                    <h5 className="text-muted pt-3">{date.toLocaleTimeString()}</h5>
+                </div>
+                <div className="col-12">
+                    <hr />
+                </div>
             </div>
-            <div className="col-12 col-12 col-md-4 col-lg-7 text-right">
-                <ul className="list-inline pt-3">
-                    <li className="list-inline-item title">หน้าแรก</li>
-                    <li className="list-inline-item title">|</li>
-                    <li className="list-inline-item title">เกี่ยวกับเรา</li>
-                </ul>
-            </div>
-            <div className="col-12 col-md-4 col-lg-1 text-right">
-                <h5 className="text-muted pt-3">{date.toLocaleTimeString()}</h5>
-            </div>
-            <div className="col-12">
-                <hr />
-            </div>
-        </div>
+        </>
     )
 }
 
